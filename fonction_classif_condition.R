8772d001d2863168af48053b4679070981080028
martin pezous <martin.pezous-puech@live.fr>
2020-06-08 09:54:56 +0200

    correction nom colonnes

--- fonction_classif_condition.R
+++ fonction_classif_condition.R
@@ -25,13 +25,14 @@ classfonction <- function(file_name.pdf, df, methode, class_target) {
     prediction = predict(training, newdata = df)
     prediction_df = as.data.frame(prediction)
     prediction_df$id = 1:nrow(prediction_df)
-    col_names = paste("prob", 1:4, sep ="")
+    #col_names = paste("prob", 1:4, sep ="")
+    col_names = c("truth", "prob3", "prob4", "response","id")
     colnames(prediction_df) = col_names
     
     
     #PLOT
     pdf(file = file_name.pdf)
-    print(ggplot(prediction_df, aes(x = prob1, y = prob3 -0.5, fill = prob2), col = "black")+
+    print(ggplot(prediction_df, aes(x = id, y = prob3 -0.5, fill = response))+
       geom_col())+
       theme_bw()
     dev.off()
@@ -41,7 +42,8 @@ classfonction <- function(file_name.pdf, df, methode, class_target) {
     return(output)
     
   }
-  ##
+  
+  #return(list(output, perfo))
 }
 
 #TEST
