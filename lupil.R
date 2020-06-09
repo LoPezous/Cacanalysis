c4a9e0c64808570c5250c255183c42ea0527d5d0
martin pezous <martin.pezous-puech@live.fr>
2020-06-09 16:54:01 +0200

    traitement de "lupil.tsv" avant utilisation dans "fonction_classif"

---
+++ lupil.R
@@ -0,0 +1,7 @@
+rm(list = ls(all = TRUE))
+options(stringsAsFactors = FALSE)
+lupil$Treatment = gsub("ILT-101", "IL2", lupil$Treatment)
+lupil$Treatment = gsub("Placebo", "PL", lupil$Treatment)
+
+lupil$sampleid = paste0(lupil$Treatment,"_",lupil$Responder,"_",lupil$ID)
+lupil$file = rownames(lupil)
