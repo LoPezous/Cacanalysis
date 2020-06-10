51273b9c583bd34eb66020cabd2c8de1660ff432
martin pezous <martin.pezous-puech@live.fr>
2020-06-10 13:00:43 +0200

    IL2/PL R/NR Sample ID

--- lupil.R
+++ lupil.R
@@ -2,6 +2,8 @@ rm(list = ls(all = TRUE))
 options(stringsAsFactors = FALSE)
 lupil$Treatment = gsub("ILT-101", "IL2", lupil$Treatment)
 lupil$Treatment = gsub("Placebo", "PL", lupil$Treatment)
-
+lupil$Responder[lupil$Responder == "TRUE"] <- "R"
+lupil$Responder[lupil$Responder == "FALSE"] <- "NR"
+lupil$sampleID = paste0(lupil$Treatment,"_",lupil$Responder,"_",lupil$ID)
 lupil$file = rownames(lupil)
-lupil = lupil[c("Treatment","file")]
+
