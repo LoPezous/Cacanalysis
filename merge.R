49951b44483ccd1dbd8fe810497a62ec2f5fd38d
martin pezous <martin.pezous-puech@live.fr>
2020-06-10 13:01:41 +0200

    flip

--- merge.R
+++ merge.R
@@ -1,3 +1,4 @@
+
 merge_OTU = function(repertoire){
   files = dir(repertoire, full.names = TRUE)
   
@@ -17,13 +18,25 @@ merge_OTU = function(repertoire){
     colnames(dfx) = col_namesdfx 
     dfx = dfx[c("species", basename(file))]
     colnames(dfx) = gsub("_profile.txt","", colnames(dfx))
+    
     df = merge(dfx, df, by = c("species"), all = TRUE)
     
   }
+  #ENLEVE RESIDUS DES COLONNES DE "DF" VIDE
   df$abundance = NULL
   df$unknown = NULL
   df$coverage = NULL
   df$nreads = NULL
+  #FLIP
+  df_duplicate = df #conserve species
+  df$species = NULL #sinon problème quand transpose
+  df = t(df) #transpose 
+  df = as.data.frame(df) #reconversion en df
+  colnames(df) = df_duplicate$species #colnames = nom OTU
+  df$file = rownames(df) #noms des profils dans une colonne
+  #
+  rownames(df) = lupil[lupil$file == df$file]$sampleID
+  
   return(df)
   
 }
