58a7861f98b6a706947527c8f38d6761db2de072
martin pezous <martin.pezous-puech@live.fr>
2020-06-11 14:57:22 +0200

    ménage

--- lupil.R
+++ lupil.R
@@ -6,6 +6,7 @@ lupil$Treatment = gsub("ILT-101", "IL2", lupil$Treatment)
 lupil$Treatment = gsub("Placebo", "PL", lupil$Treatment)
 lupil$Responder[lupil$Responder == "TRUE"] <- "R"
 lupil$Responder[lupil$Responder == "FALSE"] <- "NR"
-lupil$sampleID = paste0(lupil$Treatment,"_",lupil$Responder,"_",lupil$ID,"_",lupil$Timepoint,"_",lupil$file)
 lupil$file = rownames(lupil)
+lupil$sampleID = paste0(lupil$Treatment,"_",lupil$Responder,"_",lupil$ID,"_",lupil$Timepoint,"_",lupil$file)
+write.table(lupil, file = "lupil.txt")
 
