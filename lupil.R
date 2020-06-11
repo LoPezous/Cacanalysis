bea0dac7f5283a2dd3042732ad1cddd7b7d06397
martin pezous <martin.pezous-puech@live.fr>
2020-06-11 10:45:03 +0200

    ajout de file à sampleID (plus de duplication dans merge_OTU)

--- lupil.R
+++ lupil.R
@@ -1,5 +1,7 @@
 rm(list = ls(all = TRUE))
 options(stringsAsFactors = FALSE)
+
+#TRAITEMENT LUPIL AVANT DE LANCER MERGE_OTU
 lupil$Treatment = gsub("ILT-101", "IL2", lupil$Treatment)
 lupil$Treatment = gsub("Placebo", "PL", lupil$Treatment)
 lupil$Responder[lupil$Responder == "TRUE"] <- "R"
