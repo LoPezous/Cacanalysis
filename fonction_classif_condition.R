68cc577874402f3eadb4bfe12613663cf4b50dcf
martin pezous <martin.pezous-puech@live.fr>
2020-06-12 16:42:17 +0200

    enregistre dataframe fv$data + plot (feature values)

--- fonction_classif_condition.R
+++ fonction_classif_condition.R
@@ -14,7 +14,16 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
   fv = generateFilterValuesData(task, method = feature_selection_method )
   filtered.task = filterFeatures(task, fval = fv, perc = filter_perc)
   print(fv)
-  
+  fv_data = fv$data
+  fvdf = as.data.frame(fv$data)
+  print(fvdf)
+  #PLOT FILTER VALUES
+  pdf(file = "fv.pdf")
+  print(ggplot(fvdf, aes(x= name, y = value))+
+    geom_col()+
+      theme_bw())
+  dev.off()
+  write.table(x=fvdf, file = "filteredvalues.txt")
   #LEARNER
   methoddf = listLearners("classif", properties = c("prob"))
   '%notin%' <- Negate('%in%')
@@ -25,6 +34,7 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
     
     base_learner = makeLearner(method, predict.type = "prob")
     learner = makeFilterWrapper(learner = base_learner, fw.method = feature_selection_method, fw.perc = filter_perc)
+    
     
     #TRAINING
     training = train(learner, filtered.task)
@@ -43,14 +53,14 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
     #PLOT
     pdf(file = file_name.pdf)
     print(ggplot(prediction_df, aes(x = id, y = prob.class -0.5, fill = truth))+
-      geom_col())+
-      theme_bw()
+      geom_col()+
+      theme_bw())
     dev.off()
-    print(print(ggplot(prediction_df, aes(x = id, y = prob.class -0.5, fill = truth))+
-                  geom_col())+
+    print(ggplot(prediction_df, aes(x = id, y = prob.class -0.5, fill = truth))+
+                  geom_col()+
             theme_bw())
     #OUTPUT
-    output = write.table(prediction_df)
+    output = prediction_df
     
     return(output)
     
@@ -60,6 +70,6 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
 
 
 #TEST
-classfonction("traitement.pdf", TLUPI ,"FSelectorRcpp_information.gain", 0.25, "classif.glmnet", "treatment")
+classfonction("traitement.pdf", TLUPI ,"anova.test", 0.999, "classif.glmnet", "treatment")
 
 
