b4b3340aaa604677cc3f461c720ed9d1d3417a6a
martin pezous <martin.pezous-puech@live.fr>
2020-06-16 10:09:49 +0200

    adaptation mulitmethods

--- fonction_classif_condition.R
+++ fonction_classif_condition.R
@@ -13,17 +13,16 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
   #FEATURE SELECTION
   fv = generateFilterValuesData(task, method = feature_selection_method )
   filtered.task = filterFeatures(task, fval = fv, perc = filter_perc)
-  print(fv)
   fv_data = fv$data
   fvdf = as.data.frame(fv$data)
   print(fvdf)
   #PLOT FILTER VALUES
-  pdf(file = "fv.pdf")
+  pdf(file = paste0(feature_selection_method,"_","fv.pdf"))
   print(ggplot(fvdf, aes(x= name, y = value))+
     geom_col()+
       theme_bw())
   dev.off()
-  write.table(x=fvdf, file = "filteredvalues.txt")
+  write.table(x=fvdf, file = paste0(feature_selection_method,"_","fv.txt"))
   #LEARNER
   methoddf = listLearners("classif", properties = c("prob"))
   '%notin%' <- Negate('%in%')
@@ -38,7 +37,7 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
     
     #TRAINING
     training = train(learner, filtered.task)
-    print(getFilteredFeatures(training))
+    print(getFilteredFeatures(training)) 
     #PREDICTION
     prediction = predict(training, newdata = df)
     prediction_df = as.data.frame(prediction)
@@ -51,7 +50,7 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
     print(performance(prediction))
     
     #PLOT
-    pdf(file = file_name.pdf)
+    pdf(file = paste0(feature_selection_method,"_",method,"_",file_name.pdf))
     print(ggplot(prediction_df, aes(x = id, y = prob.class -0.5, fill = truth))+
       geom_col()+
       theme_bw())
@@ -70,6 +69,6 @@ classfonction <- function(file_name.pdf, df, feature_selection_method, filter_pe
 
 
 #TEST
-classfonction("traitement.pdf", TLUPI ,"anova.test", 0.1, "classif.glmnet", "treatment")
+classfonction("traitement.pdf", TLUPI ,"anova.test", 0.8, "classif.glmnet", "treatment")
 
 
